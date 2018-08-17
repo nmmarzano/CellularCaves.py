e0aff39575424945ff3722e62eefe889f5a83be0
GaoGaoTaiga <nmmarzano@users.noreply.github.com>
2018-08-16 22:56:46 -0300

    Add retries for floodings below a set percentage

--- cellularcaves.py
+++ cellularcaves.py
@@ -45,31 +45,37 @@ def automataIteration(grid, minCount, makePillars):
                 new_grid[i][j]=0
     return new_grid
 
-def floodFindEmpty(grid):
-    new_grid = [[1 for x in range(len(grid[0]))] for y in range(len(grid))]
-    unvisited = deque([])
-    open_count = 0
+def floodFindEmpty(grid, tries, goal):
+    times_remade = 0
+    percentage = 0
 
-    #find a random empty space, hope it's the biggest cave
-    randx = random.randint(0,len(grid)-1)
-    randy = random.randint(0,len(grid[0])-1)
-    while(grid[randx][randy] == 1):
+    while times_remade<tries and percentage<goal:
+        copy_grid = [row[:] for row in grid]
+        open_count = 0
+        times_remade+=1
+        unvisited = deque([])
+        new_grid = [[1 for x in range(len(grid[0]))] for y in range(len(grid))]
+        #find a random empty space, hope it's the biggest cave
         randx = random.randint(0,len(grid)-1)
         randy = random.randint(0,len(grid[0])-1)
-
-    unvisited.append([randx, randy])
-    while len(unvisited)>0:
-        current = unvisited.popleft()
-        new_grid[current[0]][current[1]] = 0
-        for k in range(-1,2):
-            for l in range(-1,2):
-                if current[0]+k >= 0 and current[0]+k<len(grid) and current[1]+l >= 0 and current[1]+l < len(grid[0]): #if we're not out of bounds
-                    if grid[current[0]+k][current[1]+l]==0: #if it's an empty space
-                        grid[current[0]+k][current[1]+l]=2 #mark visited
-                        open_count += 1
-                        unvisited.append([current[0]+k, current[1]+l])
-    
-    return new_grid, open_count
+        while(grid[randx][randy] == 1):
+            randx = random.randint(0,len(grid)-1)
+            randy = random.randint(0,len(grid[0])-1)
+        unvisited.append([randx, randy])
+        while len(unvisited)>0:
+            current = unvisited.popleft()
+            new_grid[current[0]][current[1]] = 0
+            for k in range(-1,2):
+                for l in range(-1,2):
+                    if current[0]+k >= 0 and current[0]+k<len(grid) and current[1]+l >= 0 and current[1]+l < len(grid[0]): #if we're not out of bounds
+                        if copy_grid[current[0]+k][current[1]+l]==0: #if it's an empty space
+                            copy_grid[current[0]+k][current[1]+l]=2 #mark visited
+                            open_count += 1
+                            unvisited.append([current[0]+k, current[1]+l])
+        percentage = open_count*100/(len(grid)*len(grid[0]))
+        print("counted {0}, {1}%...".format(open_count,percentage))
+        
+    return new_grid, percentage
 
 def main():
     width = int(input("Enter the width: "))
@@ -80,6 +86,8 @@ def main():
     count = 5
     iterations = int(input("Enter the number of regular iterations: "))
     pillarIterations = int(input("Enter the number of pillar-generating iterations: "))
+    floodTries = 5
+    goalPercentage = 30 #above 30% seems to be a good target
 
     grid = makeGrid(width, height)
     
@@ -98,9 +106,12 @@ def main():
         printGrid(grid, '# ', '· ')
 
     print("\nAfter flood algorithm to find the biggest cave:")
-    grid, open_count = floodFindEmpty(grid)
-    printGrid(grid, '# ', '· ')
-    print("Percentage of open space: {0}%".format(open_count*100/(width*height))) #above 40% seems to be a good target
+    grid, percentage = floodFindEmpty(grid, floodTries, goalPercentage)
+    if percentage<goalPercentage:
+        print("Failed to produce a big enough cave after {0} tries...".format(floodTries))
+    else:
+        print("Percentage of open space: {0}%".format(percentage)) 
+        printGrid(grid, '# ', '· ')
 
     print("")
     main()
